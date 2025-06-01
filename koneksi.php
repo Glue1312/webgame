7452c5ecfd96106e1210c5cc1ad1046ad3e1b41d
Glue1312 <127171088+Glue1312@users.noreply.github.com>
2025-06-02 00:27:26 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -3,7 +3,7 @@
 $db_socket = getenv('DB_SOCKET') ?: '/cloudsql/e-03-452916:us-central1:gamebos'; // Sesuaikan dengan Instance connection name Anda
 $db_name = getenv('DB_NAME') ?: 'gamebosdb'; // Nama database Anda dari pa_web.sql
 $db_user = getenv('DB_USER') ?: 'ester';       // Username database Anda
-$db_pass = getenv('DB_PASS') ?: 'PASSWORD_KUAT_ANDA'; // GANTI DENGAN PASSWORD KUAT YANG SUDAH ANDA BUAT
+$db_pass = getenv('DB_PASS') ?: ''; // GANTI DENGAN PASSWORD KUAT YANG SUDAH ANDA BUAT
 
 $connect = new mysqli(null, $db_user, $db_pass, $db_name, null, $db_socket);
 
