b357203a66771591203aa6d37ecc76f155accf4a
Glue1312 <127171088+Glue1312@users.noreply.github.com>
2025-06-02 00:20:48 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
-<?php  
+<?php
 	session_start();
 	session_destroy();
 
-	header("location:login.php?pesan=logout")
+	header("location: index.php?page=login&pesan=logout"); // Perubahan di sini
 ?>
